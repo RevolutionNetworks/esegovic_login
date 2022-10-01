f27d13f6c686d632c94ed097eb154552497b0e0a
Leon Gerrist <66485249+RevolutionNetworks@users.noreply.github.com>
2022-10-01 18:31:39 +0200

    Whitelist Add

--- SQL/sql.sql
+++ SQL/sql.sql
@@ -3,5 +3,6 @@ CREATE TABLE IF NOT EXISTS `esegovic_login` (
   `username` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `password` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `secretKey` varchar(4) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `whitelist` tinyint(4) DEFAULT 0,
   PRIMARY KEY (`identifier`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
